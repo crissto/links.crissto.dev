83f14cc6cf60b545f5191c487dfffe39340bb61e
Christian Stoyanov <cristian.stoyanov@gmail.com>
2022-09-10 21:51:40 +0100

    Posthog

--- pages/[...link].tsx
+++ pages/[...link].tsx
@@ -1,28 +1,40 @@
-import type { GetServerSidePropsContext } from 'next'
-import links from '../links.json'
+import { randomUUID } from "crypto";
+import type { GetServerSidePropsContext } from "next";
+import { PostHog } from "posthog-node";
+import links from "../links.json";
 
 export default function Link({ link }: { link: string }) {
-  return <p>{link}</p>
+  return <p>{link}</p>;
 }
 
+export const getServerSideProps = async ({
+  params,
+}: GetServerSidePropsContext) => {
+  console.log(process.env);
 
-export const getServerSideProps = async ({params}: GetServerSidePropsContext) => {
-    
-    if (params?.link) {
-        return {
-            redirect: {
-                destination: (links as Record<string, string>)[params.link as string],
-                permanent: false
-            }
-        }
-    }
+  new PostHog(process.env.POSTHOG_API_KEY!, {
+    host: process.env.POSTHOG_URL,
+  }).capture({
+    distinctId: randomUUID(),
+    event: "pageview",
+    properties: {
+      path: params?.link,
+    },
+  });
 
+  if (params?.link) {
     return {
-        redirect: {
-            destination: "https://crissto.dev",
-            permanent: false
-        }
-    }
-}
+      redirect: {
+        destination: (links as Record<string, string>)[params.link as string],
+        permanent: false,
+      },
+    };
+  }
 
-
+  return {
+    redirect: {
+      destination: "https://crissto.dev",
+      permanent: false,
+    },
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,48 @@
-import { type GetServerSidePropsContext } from "next"
+import * as cookie from "cookie";
+import { randomUUID } from "crypto";
+import { type GetServerSidePropsContext } from "next";
+import { PostHog } from "posthog-node";
 
 export default function Home() {
-    return (
-        <div>Home</div>
-    )
+  return <div>Home</div>;
 }
 
-export const getServerSideProps = async ({params}: GetServerSidePropsContext) => {
+export const getServerSideProps = async ({
+  params,
+  req,
+  res,
+}: GetServerSidePropsContext) => {
+  const cookies = req.headers.cookie;
 
-    return {
-        redirect: {
-            destination: "https://crissto.dev",
-            permanent: false
-        }
-    }
-}
+  const parsedCookies = cookie.parse(cookies || "");
+
+  const distinctId = parsedCookies.distinctId || randomUUID();
+
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("distinctId", distinctId, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 7 * 30, // 1 month
+      path: "/",
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    })
+  );
+
+  new PostHog(process.env.POSTHOG_API_KEY!, {
+    host: process.env.POSTHOG_URL,
+  }).capture({
+    distinctId: randomUUID(),
+    event: "$pageview",
+    properties: {
+      path: params?.link,
+    },
+  });
+
+  return {
+    redirect: {
+      destination: "https://crissto.dev",
+      permanent: false,
+    },
+  };
+};
